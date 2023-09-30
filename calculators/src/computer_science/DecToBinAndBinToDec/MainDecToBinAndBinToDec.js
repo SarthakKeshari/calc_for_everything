82a2abb889ce3c098090e2b66327c68adb58ac13
SarthakKeshari <srthkkeshari84@gmail.com>
2023-10-01 01:32:26 +0530

    Added folders for contribution

--- calculators/src/computer_science/DecToBinAndBinToDec/MainDecToBinAndBinToDec.js
+++ calculators/src/computer_science/DecToBinAndBinToDec/MainDecToBinAndBinToDec.js
@@ -1,5 +1,14 @@
+import React from 'react';
+import { Container, Typography } from '@mui/material';
+
 function MainDecToBinAndBinToDec(){
-    
+    return(
+        <Container maxWidth="lg" sx={{ bgcolor: '#eeeeee', minHeight: '90vh', paddingY:"10" }}>
+            <Typography pt={1} variant='h5' sx = {{textAlign: "center"}}>Decimal to Binary and Binary to Decimal Calculator</Typography>
+            <hr/>
+            <br/>
+        </Container>
+    )
 }
 
 export default MainDecToBinAndBinToDec;
--- calculators/src/computer_science/HexToBinAndBinToHex/MainHexToBinAndBinToHex.js
+++ calculators/src/computer_science/HexToBinAndBinToHex/MainHexToBinAndBinToHex.js
@@ -1,5 +1,14 @@
+import React from 'react';
+import { Container, Typography } from '@mui/material';
+
 function MainHexToBinAndBinToHex(){
-    
+    return(
+        <Container maxWidth="lg" sx={{ bgcolor: '#eeeeee', minHeight: '90vh', paddingY:"10" }}>
+            <Typography pt={1} variant='h5' sx = {{textAlign: "center"}}>Hexadecimal to Binary and Binary to Hexadecimal Calculator</Typography>
+            <hr/>
+            <br/>
+        </Container>
+    )
 }
 
 export default MainHexToBinAndBinToHex;
