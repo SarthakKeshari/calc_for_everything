86c783737db192f1e0a4c6c36893d3fa8ae4aa48
Ansh <govardhanshah456@gmail.com>
2023-10-01 20:08:27 +0530

    Added Statistic Calculator

--- calculators/src/App.js
+++ calculators/src/App.js
@@ -14,6 +14,7 @@ import MainStatistics from './mathematical/statistics/MainStatistics';
 import MainBMI from './health/BMI/MainBMI';
 import MainBMR from './health/BMR/MainBMR';
 import MainBodyFat from './health/BodyFat/MainBodyFat';
+
 function App() {
   return (
     <div className="App">
