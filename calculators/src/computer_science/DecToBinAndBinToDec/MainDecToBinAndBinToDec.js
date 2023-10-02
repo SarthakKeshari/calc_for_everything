6d00a9a69c2889eb8cc3172018f01236ca073ee1
ABHIJITH HEGDE <60565384+abhijithhegde@users.noreply.github.com>
2023-10-02 21:11:14 +0530

    added decimal to binary convertor

--- calculators/src/computer_science/DecToBinAndBinToDec/MainDecToBinAndBinToDec.js
+++ calculators/src/computer_science/DecToBinAndBinToDec/MainDecToBinAndBinToDec.js
@@ -1,14 +1,71 @@
-import React from 'react';
-import { Container, Typography } from '@mui/material';
+// src/BinaryDecimalConverter.js
+import React, { useState } from "react";
+import { Container, Typography, TextField, Button } from "@mui/material";
 
-function MainDecToBinAndBinToDec(){
-    return(
-        <Container maxWidth="lg" sx={{ bgcolor: '#eeeeee', minHeight: '90vh', paddingY:"10" }}>
-            <Typography pt={1} variant='h5' sx = {{textAlign: "center"}}>Decimal to Binary and Binary to Decimal Calculator</Typography>
-            <hr/>
-            <br/>
-        </Container>
-    )
+function BinaryDecimalConverter() {
+	const [binaryInput, setBinaryInput] = useState("");
+	const [decimalInput, setDecimalInput] = useState("");
+
+	const handleBinaryChange = (event) => {
+		setBinaryInput(event.target.value);
+	};
+
+	const handleDecimalChange = (event) => {
+		setDecimalInput(event.target.value);
+	};
+
+	const convertBinaryToDecimal = () => {
+		const decimalValue = parseInt(binaryInput, 2);
+		if (!isNaN(decimalValue)) {
+			setDecimalInput(decimalValue.toString());
+		} else {
+			setDecimalInput("Invalid input");
+		}
+	};
+
+	const convertDecimalToBinary = () => {
+		const decimalValue = parseInt(decimalInput, 10);
+		if (!isNaN(decimalValue)) {
+			setBinaryInput(decimalValue.toString(2));
+		} else {
+			setBinaryInput("Invalid input");
+		}
+	};
+
+	return (
+		<Container
+			maxWidth="lg"
+			sx={{ bgcolor: "#eeeeee", minHeight: "90vh", paddingY: "10" }}
+		>
+			<Typography pt={1} variant="h5" sx={{ textAlign: "center" }}>
+				Decimal to Binary and Binary to Decimal Calculator
+			</Typography>
+			<hr />
+			<br />
+			<div>
+				<TextField
+					label="Binary"
+					type="text"
+					value={binaryInput}
+					onChange={handleBinaryChange}
+				/>
+				<Button variant="contained" onClick={convertBinaryToDecimal}>
+					Convert to Decimal
+				</Button>
+			</div>
+			<div>
+				<TextField
+					label="Decimal"
+					type="text"
+					value={decimalInput}
+					onChange={handleDecimalChange}
+				/>
+				<Button variant="contained" onClick={convertDecimalToBinary}>
+					Convert to Binary
+				</Button>
+			</div>
+		</Container>
+	);
 }
 
-export default MainDecToBinAndBinToDec;
+export default BinaryDecimalConverter;
