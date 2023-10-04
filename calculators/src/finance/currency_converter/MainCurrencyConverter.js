3e27fad4fba5d9da03f6274d4e486af90ba070db
Musak2 <63206033+Musak2@users.noreply.github.com>
2023-10-04 20:06:21 +0200

    Currency converter
    Added calculator for currency conversion by two files of .js and .css.

--- calculators/src/finance/currency_converter/MainCurrencyConverter.js
+++ calculators/src/finance/currency_converter/MainCurrencyConverter.js
@@ -1,12 +1,89 @@
 import React from 'react';
 import { Container, Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
+import Axios from 'axios';
+import Dropdown from 'react-dropdown';
+import { HiSwitchHorizontal } from 'react-icons/hi';
+import 'react-dropdown/style.css';
+import './CurrencyApp.css';
+
 
 function MainCurrencyConverter(){
+
+    // Initializing all the state variables
+    const [info, setInfo] = useState([]);
+    const [input, setInput] = useState(0);
+    const [from, setFrom] = useState("usd");
+    const [to, setTo] = useState("inr");
+    const [options, setOptions] = useState([]);
+    const [output, setOutput] = useState(0);
+ 
+    // Calling the api whenever the dependency changes
+    useEffect(() => {
+        Axios.get(
+`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${from}.json`)
+            .then((res) => {
+                setInfo(res.data[from]);
+            })
+    }, [from]);
+ 
+    // Calling the convert function whenever
+    // a user switches the currency
+    useEffect(() => {
+        setOptions(Object.keys(info));
+        convert();
+    }, [info])
+ 
+    // Function to convert the currency
+    function convert() {
+        var rate = info[to];
+        setOutput(input * rate);
+    }
+ 
+    // Function to switch between two currency
+    function flip() {
+        var temp = from;
+        setFrom(to);
+        setTo(temp);
+    }
+
     return(
         <Container maxWidth="lg" sx={{ bgcolor: '#eeeeee', minHeight: '90vh', paddingY:"10" }}>
             <Typography pt={1} variant='h5' sx = {{textAlign: "center"}}>Currency Converter</Typography>
             <hr/>
             <br/>
+
+            <div className="container">
+                <div className="left">
+                    <h3>Amount</h3>
+                    <input type="text"
+                        placeholder="Enter the amount"
+                        onChange={(e) => setInput(e.target.value)} />
+                </div>
+                <div className="middle">
+                    <h3>From</h3>
+                    <Dropdown options={options}
+                        onChange={(e) => { setFrom(e.value) }}
+                        value={from} placeholder="From" />
+                </div>
+                <div className="switch">
+                    <HiSwitchHorizontal size="30px"
+                        onClick={() => { flip() }} />
+                </div>
+                <div className="right">
+                    <h3>To</h3>
+                    <Dropdown options={options}
+                        onChange={(e) => { setTo(e.value) }}
+                        value={to} placeholder="To" />
+                </div>
+            </div>
+            <div className="result">
+                <button onClick={() => { convert() }}>Convert</button>
+                <h2>Converted Amount:</h2>
+                <p>{input + " " + from + " = " + output.toFixed(2) + " " + to}</p>
+ 
+            </div>
+
         </Container>
     )
 }
