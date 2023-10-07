dfd536d4ef21ffd04797720cf9f897f4a857e674
SarthakKeshari <srthkkeshari84@gmail.com>
2023-10-07 11:18:12 +0530

    Added folders for contribution

--- calculators/src/computer_science/fracToDecAndDecToFrac/MainFracToDecAndDecToFrac.js
+++ calculators/src/computer_science/fracToDecAndDecToFrac/MainFracToDecAndDecToFrac.js
@@ -4,7 +4,7 @@ import { Container, Typography } from '@mui/material';
 function MainFracToDecAndDecToFrac(){
     return(
         <Container maxWidth="lg" sx={{ bgcolor: '#eeeeee', minHeight: '90vh', paddingY:"10" }}>
-            <Typography pt={1} variant='h5' sx = {{textAlign: "center"}}>Fraction To Decimal And Vice Versa</Typography>
+            <Typography pt={1} variant='h5' sx = {{textAlign: "center"}}>Fraction To Decimal And Decimal to Fraction Calculator</Typography>
             <hr/>
             <br/>
         </Container>
