62f8169a31a13d1fd9e4924e73a39cc7195eb7f5
remonix <akaastiksonkar@gmail.com>
2023-10-09 01:51:14 +0530

    added percentage calcuator

--- calculators/src/mathematical/percentageCalc/MainPercentageCalc.js
+++ calculators/src/mathematical/percentageCalc/MainPercentageCalc.js
@@ -1,12 +1,49 @@
-import React from 'react';
-import { Container, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Container, Typography, Button, TextField } from '@mui/material';
 
-function MainPercentageCalc(){
-    return(
-        <Container maxWidth="lg" sx={{ bgcolor: '#eeeeee', minHeight: '90vh', paddingY:"10" }}>
-            <Typography pt={1} variant='h5' sx = {{textAlign: "center"}}>Percentage Calculator</Typography>
-            <hr/>
-            <br/>
+function MainPercentageCalc() {
+    const [number, setNumber] = useState('');
+    const [percentage, setPercentage] = useState('');
+    const [result, setResult] = useState(null);
+
+    const calculatePercentage = () => {
+        const parsedNumber = parseFloat(number);
+        const parsedPercentage = parseFloat(percentage);
+
+        if (!isNaN(parsedNumber) && !isNaN(parsedPercentage)) {
+            const calculatedPercentage = (parsedPercentage / 100) * parsedNumber;
+            setResult(calculatedPercentage);
+        } else {
+            setResult(null);
+        }
+    };
+    return (
+        <Container maxWidth="lg" sx={{ bgcolor: '#eeeeee', minHeight: '90vh', paddingY: "10" }}>
+            <Typography pt={1} variant='h5' sx={{ textAlign: "center" }}>Percentage Calculator</Typography>
+            <hr />
+            <br />
+            <div>
+                <TextField
+                    label="Enter a number"
+                    variant="outlined"
+                    value={number}
+                    onChange={(e) => setNumber(e.target.value)}
+                />
+                <TextField
+                    label="Enter a percentage"
+                    variant="outlined"
+                    value={percentage}
+                    onChange={(e) => setPercentage(e.target.value)}
+                />
+                <Button variant="contained" color="primary" onClick={calculatePercentage}>
+                    Calculate
+                </Button>
+                {result !== null && (
+                    <div>
+                        <p>Result: {result.toFixed(2)}</p>
+                    </div>
+                )}
+            </div>
         </Container>
     )
 }
