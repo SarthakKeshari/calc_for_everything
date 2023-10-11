4cecb9f853b80f7f8568973b801b1054525dd905
Ronak-Singh <srronaksingh@gmail.com>
2023-10-11 22:25:50 +0530

    Update MainMetricUnitConverter.js

--- calculators/src/mathematical/metricUnitConverter/MainMetricUnitConverter.js
+++ calculators/src/mathematical/metricUnitConverter/MainMetricUnitConverter.js
@@ -1,21 +1,12 @@
 import React, { useState } from 'react';
-import {
-  Container,
-  Typography,
-  TextField,
-  Select,
-  MenuItem,
-  Button,
-  Box,
-} from '@mui/material';
+import { Container, Typography, TextField, FormControl, InputLabel, Select, MenuItem, Button } from '@mui/material';
 
-function MainMetricUnitConverter() {
+function MetricUnitConverter() {
   const [unitType, setUnitType] = useState('length');
   const [fromUnit, setFromUnit] = useState('meter');
   const [toUnit, setToUnit] = useState('kilometer');
   const [inputValue, setInputValue] = useState('');
   const [result, setResult] = useState('');
-  const [isReverse, setIsReverse] = useState(false); // Define isReverse state
 
   const unitData = {
     length: {
@@ -53,22 +44,13 @@ function MainMetricUnitConverter() {
     setInputValue(event.target.value);
   };
 
-  const handleToggleReverse = () => {
-    setIsReverse(!isReverse); // Toggle between forward and reverse conversions
-  };
-
   const convertUnits = () => {
     const fromFactor = unitData[unitType][fromUnit];
     const toFactor = unitData[unitType][toUnit];
     const inputValueFloat = parseFloat(inputValue);
 
     if (!isNaN(inputValueFloat)) {
-      let resultValue;
-      if (!isReverse) {
-        resultValue = (inputValueFloat * fromFactor) / toFactor; // Forward conversion
-      } else {
-        resultValue = (inputValueFloat * toFactor) / fromFactor; // Reverse conversion
-      }
+      const resultValue = (inputValueFloat * toFactor) / fromFactor;
       setResult(resultValue.toFixed(4));
     } else {
       setResult('Invalid input');
@@ -76,57 +58,49 @@ function MainMetricUnitConverter() {
   };
 
   return (
-    <Container maxWidth="lg" sx={{ bgcolor: '#eeeeee', minHeight: '90vh', paddingY: '10' }}>
-      <Typography pt={1} variant="h5" sx={{ textAlign: 'center' }}>
+    <Container maxWidth="md" sx={{ marginTop: '2rem' }}>
+      <Typography variant="h4" gutterBottom>
         Metric Unit Converter
       </Typography>
-      <hr />
       <br />
-      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        <div>
-          <Typography variant="h6">Unit Type:</Typography>
-          <Select value={unitType} onChange={handleUnitTypeChange}>
-            <MenuItem value="length">Length</MenuItem>
-            <MenuItem value="weight">Weight</MenuItem>
-          </Select>
-        </div>
-        <div>
-          <Typography variant="h6">From Unit:</Typography>
-          <Select value={fromUnit} onChange={handleFromUnitChange}>
-            {Object.keys(unitData[unitType]).map((unit) => (
-              <MenuItem key={unit} value={unit}>
-                {unit}
-              </MenuItem>
-            ))}
-          </Select>
-        </div>
-        <div>
-          <Typography variant="h6">To Unit:</Typography>
-          <Select value={toUnit} onChange={handleToUnitChange}>
-            {Object.keys(unitData[unitType]).map((unit) => (
-              <MenuItem key={unit} value={unit}>
-                {unit}
-              </MenuItem>
-            ))}
-          </Select>
-        </div>
-      </Box>
+      <FormControl fullWidth variant="outlined" sx={{ marginBottom: '1rem' }}>
+        <InputLabel>Select Unit Type</InputLabel>
+        <Select value={unitType} onChange={handleUnitTypeChange}>
+          <MenuItem value="length">Length</MenuItem>
+          <MenuItem value="weight">Weight</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl fullWidth variant="outlined" sx={{ marginBottom: '1rem' }}>
+        <InputLabel>From Unit</InputLabel>
+        <Select value={fromUnit} onChange={handleFromUnitChange}>
+          {Object.keys(unitData[unitType]).map((unit) => (
+            <MenuItem key={unit} value={unit}>
+              {unit}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      <FormControl fullWidth variant="outlined" sx={{ marginBottom: '1rem' }}>
+        <InputLabel>To Unit</InputLabel>
+        <Select value={toUnit} onChange={handleToUnitChange}>
+          {Object.keys(unitData[unitType]).map((unit) => (
+            <MenuItem key={unit} value={unit}>
+              {unit}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
       <TextField
         label="Enter Value"
         variant="outlined"
         fullWidth
         value={inputValue}
         onChange={handleInputChange}
-        sx={{ marginTop: '1rem' }}
+        sx={{ marginBottom: '1rem' }}
       />
-      <Box sx={{ display: 'flex', justifyContent: 'space-between', marginTop: '1rem' }}>
-        <Button variant="contained" onClick={convertUnits}>
-          {isReverse ? 'Reverse Convert' : 'Convert'}
-        </Button>
-        <Button variant="outlined" onClick={handleToggleReverse}>
-          Toggle Reverse
-        </Button>
-      </Box>
+      <Button variant="contained" onClick={convertUnits}>
+        Convert
+      </Button>
       <Typography variant="h6" sx={{ marginTop: '1rem' }}>
         Result: {result}
       </Typography>
@@ -134,4 +108,4 @@ function MainMetricUnitConverter() {
   );
 }
 
-export default MainMetricUnitConverter;
+export default MetricUnitConverter;
