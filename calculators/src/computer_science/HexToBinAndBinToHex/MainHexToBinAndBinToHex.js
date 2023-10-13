bc0401f62d07e97ec142fd7493b43a8df818bb92
Tushar <tushardev.work@gmail.com>
2023-10-13 21:10:12 +0530

    modified the code for floating hexadecimal values

--- calculators/src/computer_science/HexToBinAndBinToHex/MainHexToBinAndBinToHex.js
+++ calculators/src/computer_science/HexToBinAndBinToHex/MainHexToBinAndBinToHex.js
@@ -1,80 +1,137 @@
-import React, { useState } from 'react';
-import { Container, Typography, Input, Divider } from '@mui/material';
-import CopyValue from '../../components/CopyValue';
+import React, { useState } from "react";
+import {
+  TextField,
+  Typography,
+  Box,
+  Container,
+  Grid,
+  Paper,
+} from "@mui/material";
 
-function MainHexToBinAndBinToHex() {
-    // State variables to store the Hex and Binary values.
-    const [hex, setHex] = useState('');
-    const [binary, setBinary] = useState('');
-// Function to handle changes in the Hexadecimal input.
-    const handleHexChange = (e) => {
-        const hexValue = e.target.value.toUpperCase();
-        setHex(hexValue);
+function Converter() {
+  const [hexValue, setHexValue] = useState("");
+  const [binaryValue, setBinaryValue] = useState("");
 
-        // Convert Hexadecimal to Binary.
-        if (hexValue) {
-            let binValue = parseInt(hexValue, 16).toString(2);
-            setBinary(binValue);
-        } else {
-            setBinary('');
-        }
-    };
+  const handleHexChange = (event) => {
+    const inputValue = event.target.value;
+    setHexValue(inputValue);
+    setBinaryValue(hexToBinary(inputValue));
+  };
 
-    // Function to handle changes in the Binary input.
-    const handleBinaryChange = (e) => {
-        const binValue = e.target.value;
-        setBinary(binValue);
+  const handleBinaryChange = (event) => {
+    const inputValue = event.target.value;
+    setBinaryValue(inputValue);
+    setHexValue(binaryToHex(inputValue));
+  };
 
-        // Convert Binary to Hexadecimal.
-        if (binValue) {
-            let hexValue = parseInt(binValue, 2).toString(16).toUpperCase();
-            setHex(hexValue);
-        } else {
-            setHex('');
-        }
-    };
+  const hexToBinary = (hex) => {
+    if (!hex.match(/^[0-9a-fA-F.]+$/)) {
+      return "Invalid Hex";
+    }
 
-    return (
-        // Main container for the converter UI.
-        <Container maxWidth="lg" sx={{ bgcolor: '#eeeeee', minHeight: '90vh', paddingY: "10" }}>
-            <Typography pt={1} variant='h5' sx={{ textAlign: "center" }}>Hexadecimal to Binary and Binary to Hexadecimal Calculator</Typography>
-            <Divider sx={{ my: 3 }} />
-            
-            <Typography variant="h6" mt={3}>Enter Hexadecimal:</Typography>
-            <Input
-                placeholder="Hexadecimal"
-                variant="outlined"
-                type='text'
-                onChange={handleHexChange}
-                value={hex}
-                fullWidth
-                sx={{ marginBottom: 3 }}
-            />
+    const isNegative = hex[0] === "-";
+    hex = hex.replace("-", "");
 
-           
-            <Typography variant="h6">Enter Binary:</Typography>
-            <Input
-                placeholder="Binary"
-                variant="outlined"
-                type='text'
-                onChange={handleBinaryChange}
-                value={binary}
-                fullWidth
-                sx={{ marginBottom: 3 }}
-            />
+    const parts = hex.split(".");
+    const integerPart = parts[0];
+    const fractionalPart = parts[1] || "0";
 
-            <Typography variant='h6' mt={2}>Converted Hexadecimal:</Typography>
-            <div className='output'>
-                <Typography variant='body1' sx={{ color: "blue", marginBottom: 3 }}>{hex}</Typography>
-                <CopyValue value={hex} />
-            </div>
-            <Typography variant='h6'>Converted Binary:</Typography>
-            <div className='output'>
-                <Typography variant='body1' sx={{ color: "blue" }}>{binary}</Typography>
-                <CopyValue value={binary} />
-            </div>
-        </Container>
-    )
+    const integerBinary = parseInt(integerPart, 16).toString(2);
+    const fractionalBinary = parseInt(fractionalPart, 16)
+      .toString(2)
+      .padStart(fractionalPart.length * 4, "0");
+
+    let result = isNegative ? "-" : "";
+    result += integerBinary;
+
+    if (fractionalPart !== "0") {
+      result += "." + fractionalBinary;
+    }
+
+    return result;
+  };
+
+  const binaryToHex = (binary) => {
+    if (!binary.match(/^[01.]+$/)) {
+      return "Invalid Binary";
+    }
+
+    const isNegative = binary[0] === "-";
+    binary = binary.replace("-", "");
+
+    const parts = binary.split(".");
+    const integerPart = parts[0];
+    const fractionalPart = parts[1] || "0";
+
+    const integerHex = parseInt(integerPart, 2).toString(16);
+    let fractionalHex = "";
+    if (fractionalPart !== "0") {
+      const fractionalBinary = "1" + fractionalPart;
+      fractionalHex = parseInt(fractionalBinary, 2).toString(16).slice(1);
+    }
+
+    let result = isNegative ? "-" : "";
+    result += integerHex;
+
+    if (fractionalHex !== "") {
+      result += "." + fractionalHex;
+    }
+
+    return result;
+  };
+
+  return (
+    <div style={{ display: "flex", alignItems: "center", minHeight: "30vh" }}>
+      <Container>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
+            <Paper elevation={3}>
+              <Box p={2}>
+                <Typography variant="h5" align="center">
+                  Hexadecimal to Binary
+                </Typography>
+                <TextField
+                  label="Hexadecimal"
+                  variant="outlined"
+                  fullWidth
+                  value={hexValue}
+                  onChange={handleHexChange}
+                  placeholder="Enter Hexadecimal"
+                />
+                <Box mt={2}>
+                  <Typography variant="h6">
+                    <strong>Binary:</strong> {binaryValue}
+                  </Typography>
+                </Box>
+              </Box>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Paper elevation={3}>
+              <Box p={2}>
+                <Typography variant="h5" align="center">
+                  Binary to Hexadecimal
+                </Typography>
+                <TextField
+                  label="Binary"
+                  variant="outlined"
+                  fullWidth
+                  value={binaryValue}
+                  onChange={handleBinaryChange}
+                  placeholder="Enter Binary"
+                />
+                <Box mt={2}>
+                  <Typography variant="h6">
+                    <strong>Hexadecimal:</strong> {hexValue}
+                  </Typography>
+                </Box>
+              </Box>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
 }
 
-export default MainHexToBinAndBinToHex;
+export default Converter;
