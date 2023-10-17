a3dc1e37901e51a3d44ae9e12b1df6a6a0a6a54a
Vidip <vidipchhabra21@gmail.com>
2023-10-17 11:46:44 +0530

    Ohm's Law calculator

--- calculators/src/physics/OhmsLawCalculator/OhmsLawCalculator.js
+++ calculators/src/physics/OhmsLawCalculator/OhmsLawCalculator.js
@@ -2,9 +2,30 @@ import React, { useState } from 'react';
 import { Container, Typography, Button } from '@mui/material';
 
 function OhmsLawCalculator() {
+  const [calculationType, setCalculationType] = useState(null);
   const [voltage, setVoltage] = useState('');
   const [current, setCurrent] = useState('');
   const [resistance, setResistance] = useState('');
+
+  const handleCalculationTypeChange = (type) => {
+    setCalculationType(type);
+    setVoltage('');
+    setCurrent('');
+    setResistance('');
+  };
+
+  const handleInputChange = (e, type) => {
+    const value = e.target.value;
+    if (type === 'voltage') setVoltage(value);
+    else if (type === 'current') setCurrent(value);
+    else if (type === 'resistance') setResistance(value);
+  };
+
+  const handleCalculation = () => {
+    if (calculationType === 'voltage') calculateVoltage();
+    else if (calculationType === 'current') calculateCurrent();
+    else if (calculationType === 'resistance') calculateResistance();
+  };
 
   const calculateVoltage = () => {
     if (current && resistance) {
@@ -27,55 +48,88 @@ function OhmsLawCalculator() {
     }
   };
 
-  const handleVoltageChange = (e) => {
-    setVoltage(e.target.value);
-  };
-
-  const handleCurrentChange = (e) => {
-    setCurrent(e.target.value);
-  };
-
-  const handleResistanceChange = (e) => {
-    setResistance(e.target.value);
-  };
-
   return (
     <Container maxWidth="lg" sx={{ bgcolor: '#eeeeee', minHeight: '90vh', paddingY: '10' }}>
       <Typography pt={1} variant="h5" sx={{ textAlign: 'center' }}>Ohm's Law Calculator</Typography>
       <hr />
       <br />
       <div style={{ textAlign: 'center' }}>
-        <label htmlFor="voltage">Voltage (V):</label>
-        <input
-          type="number"
-          id="voltage"
-          value={voltage}
-          onChange={handleVoltageChange}
-        />
+        <select value={calculationType} onChange={(e) => handleCalculationTypeChange(e.target.value)}>
+          <option value="">Select Calculation Type</option>
+          <option value="voltage">Calculate Voltage</option>
+          <option value="current">Calculate Current</option>
+          <option value="resistance">Calculate Resistance</option>
+        </select>
 
-        <label htmlFor="current">Current (A):</label>
-        <input
-          type="number"
-          id="current"
-          value={current}
-          onChange={handleCurrentChange}
-        />
+        {calculationType !== null && (
+          <div>
+            {calculationType === 'voltage' && (
+              <div>
+                <label htmlFor="current">Current (A):</label>
+                <input
+                  type="number"
+                  id="current"
+                  value={current}
+                  onChange={(e) => handleInputChange(e, 'current')}
+                />
 
-        <label htmlFor="resistance">Resistance (Ω):</label>
-        <input
-          type="number"
-          id="resistance"
-          value={resistance}
-          onChange={handleResistanceChange}
-        />
+                <label htmlFor="resistance">Resistance (Ω):</label>
+                <input
+                  type="number"
+                  id="resistance"
+                  value={resistance}
+                  onChange={(e) => handleInputChange(e, 'resistance')}
+                />
+              </div>
+            )}
 
-        <Button variant="contained" onClick={calculateVoltage}>Calculate Voltage</Button>
-        <Button variant="contained" onClick={calculateCurrent}>Calculate Current</Button>
-        <Button variant="contained" onClick={calculateResistance}>Calculate Resistance</Button>
+            {calculationType === 'current' && (
+              <div>
+                <label htmlFor="voltage">Voltage (V):</label>
+                <input
+                  type="number"
+                  id="voltage"
+                  value={voltage}
+                  onChange={(e) => handleInputChange(e, 'voltage')}
+                />
 
-        {voltage !== '' && <div>Calculated Voltage: {voltage} V</div>}
-        {current !== '' && <div>Calculated Current: {current} A</div>}
-        {resistance !== '' && <div>Calculated Resistance: {resistance} Ω</div>}
+                <label htmlFor="resistance">Resistance (Ω):</label>
+                <input
+                  type="number"
+                  id="resistance"
+                  value={resistance}
+                  onChange={(e) => handleInputChange(e, 'resistance')}
+                />
+              </div>
+            )}
+
+            {calculationType === 'resistance' && (
+              <div>
+                <label htmlFor="voltage">Voltage (V):</label>
+                <input
+                  type="number"
+                  id="voltage"
+                  value={voltage}
+                  onChange={(e) => handleInputChange(e, 'voltage')}
+                />
+
+                <label htmlFor="current">Current (A):</label>
+                <input
+                  type="number"
+                  id="current"
+                  value={current}
+                  onChange={(e) => handleInputChange(e, 'current')}
+                />
+              </div>
+            )}
+
+            <Button variant="contained" onClick={handleCalculation}>Calculate</Button>
+          </div>
+        )}
+
+        {voltage !== '' && calculationType === 'voltage' && <div>Calculated Voltage: {voltage} V</div>}
+        {current !== '' && calculationType === 'current' && <div>Calculated Current: {current} A</div>}
+        {resistance !== '' && calculationType === 'resistance' && <div>Calculated Resistance: {resistance} Ω</div>}
       </div>
     </Container>
   );
