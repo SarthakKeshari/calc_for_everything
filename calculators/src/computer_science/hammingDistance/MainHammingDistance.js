7657caaff76b662eb84b457fae274c053d1cd813
remonix <akaastiksonkar@gmail.com>
2023-10-17 22:19:46 +0530

    Added Hamming Distance CALc

--- calculators/src/computer_science/hammingDistance/MainHammingDistance.js
+++ calculators/src/computer_science/hammingDistance/MainHammingDistance.js
@@ -1,12 +1,63 @@
-import React from 'react';
-import { Container, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { TextField, Button, Typography, Container, Grid } from '@mui/material';
 
-function MainHammingDistance(){
-    return(
-        <Container maxWidth="lg" sx={{ bgcolor: '#eeeeee', minHeight: '90vh', paddingY:"10" }}>
-            <Typography pt={1} variant='h5' sx = {{textAlign: "center"}}>Hamming Distance Calculator</Typography>
-            <hr/>
-            <br/>
+function MainHammingDistance() {
+    const [string1, setString1] = useState('');
+    const [string2, setString2] = useState('');
+    const [distance, setDistance] = useState('');
+
+    const calculateHammingDistance = () => {
+        if (string1.length !== string2.length) {
+            setDistance('Strings must have the same length');
+            return;
+        }
+
+        let hammingDistance = 0;
+        for (let i = 0; i < string1.length; i++) {
+            if (string1[i] !== string2[i]) {
+                hammingDistance++;
+            }
+        }
+
+        setDistance(`Hamming Distance: ${hammingDistance}`);
+    };
+
+    return (
+        <Container maxWidth="lg" sx={{ bgcolor: '#eeeeee', minHeight: '90vh', paddingY: "10" }}>
+            <Typography pt={1} variant='h5' sx={{ textAlign: "center" }}>Hamming Distance Calculator</Typography>
+            <hr />
+
+            <br />
+            <Grid container spacing={2}>
+                <Grid item xs={6}>
+                    <TextField
+                        label="String 1"
+                        variant="outlined"
+                        fullWidth
+                        value={string1}
+                        onChange={(e) => setString1(e.target.value)}
+                    />
+                </Grid>
+                <Grid item xs={6}>
+                    <TextField
+                        label="String 2"
+                        variant="outlined"
+                        fullWidth
+                        value={string2}
+                        onChange={(e) => setString2(e.target.value)}
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <Button variant="contained" color="primary" onClick={calculateHammingDistance}>
+                        Calculate
+                    </Button>
+                </Grid>
+                <Grid item xs={12}>
+                    <Typography variant="h6" gutterBottom>
+                        {distance}
+                    </Typography>
+                </Grid>
+            </Grid>
         </Container>
     )
 }
