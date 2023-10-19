600b818ae7c327be3849252051c69efcf9b3d0dc
Sharad <20ics059@gbu.ac.in>
2023-10-19 14:10:57 +0530

    upgraded oct bin and hex dec calculator

--- calculators/src/computer_science/checksumCalc/MainChecksumCalc.js
+++ calculators/src/computer_science/checksumCalc/MainChecksumCalc.js
@@ -1,55 +1,55 @@
-import React, { useState } from 'react';
-import { Container, Typography, TextField, Button } from '@mui/material';
-import hmacSHA512 from 'crypto-js/hmac-sha512';
-import Base64 from 'crypto-js/enc-base64';
-import sha256 from 'crypto-js/sha256';
+// import React, { useState } from 'react';
+// import { Container, Typography, TextField, Button } from '@mui/material';
+// import hmacSHA512 from 'crypto-js/hmac-sha512';
+// import Base64 from 'crypto-js/enc-base64';
+// import sha256 from 'crypto-js/sha256';
     
-function MainChecksumCalc() {
-    const [inputText, setInputText] = useState('');
-    const [checksum, setChecksum] = useState('');
+// function MainChecksumCalc() {
+//     const [inputText, setInputText] = useState('');
+//     const [checksum, setChecksum] = useState('');
     
 
-    const calculateChecksum = () => {
-        if (inputText) {
-            const hashDigest = sha256(inputText);
-            const privateKey = "calc"
-            const hmacDigest = Base64.stringify(hmacSHA512(hashDigest, privateKey));
-            setChecksum(hmacDigest);
-            console.log(hmacDigest)
+//     const calculateChecksum = () => {
+//         if (inputText) {
+//             const hashDigest = sha256(inputText);
+//             const privateKey = "calc"
+//             const hmacDigest = Base64.stringify(hmacSHA512(hashDigest, privateKey));
+//             setChecksum(hmacDigest);
+//             console.log(hmacDigest)
 
           
-        }
-    };
+//         }
+//     };
 
-    return (
-        <Container maxWidth="lg" sx={{ bgcolor: '#eeeeee', minHeight: '90vh', paddingY: "10" }}>
-            <Typography pt={1} variant='h5' sx={{ textAlign: "center" }}>Checksum Calculator</Typography>
-            <hr />
-            <br />
-            <TextField
+//     return (
+//         <Container maxWidth="lg" sx={{ bgcolor: '#eeeeee', minHeight: '90vh', paddingY: "10" }}>
+//             <Typography pt={1} variant='h5' sx={{ textAlign: "center" }}>Checksum Calculator</Typography>
+//             <hr />
+//             <br />
+//             <TextField
 
-                label="Enter text for checksum calculation"
-                variant="outlined"
-                multiline
-                rows={4}
-                value={inputText}
-                onChange={(e) => setInputText(e.target.value)}
-            />
-            <Button variant="contained" color="primary" onClick={calculateChecksum}>
-                Calculate Checksum
-            </Button>
-            <br/>
-            <Typography mt={4}>
+//                 label="Enter text for checksum calculation"
+//                 variant="outlined"
+//                 multiline
+//                 rows={4}
+//                 value={inputText}
+//                 onChange={(e) => setInputText(e.target.value)}
+//             />
+//             <Button variant="contained" color="primary" onClick={calculateChecksum}>
+//                 Calculate Checksum
+//             </Button>
+//             <br/>
+//             <Typography mt={4}>
                 
-                {
-                    checksum &&
-                    `SHA-256 : ${checksum}`
+//                 {
+//                     checksum &&
+//                     `SHA-256 : ${checksum}`
 
-                }
-            </Typography>
+//                 }
+//             </Typography>
            
-        </Container>
-    );
-};
+//         </Container>
+//     );
+// };
 
-export default MainChecksumCalc;
+// export default MainChecksumCalc;
