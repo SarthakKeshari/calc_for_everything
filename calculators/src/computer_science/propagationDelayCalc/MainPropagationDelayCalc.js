4ed79bc366cbf827fbc48aa588988bcb57294948
Syed Hisham Akmal <hisham0502@gmail.com>
2023-10-22 00:22:02 +0530

    Revert "Enhanced error handling"
    This reverts commit cbd4466f7b277f3bc7015f8d21bc0e63fe40e2b3.

--- calculators/src/computer_science/propagationDelayCalc/MainPropagationDelayCalc.js
+++ calculators/src/computer_science/propagationDelayCalc/MainPropagationDelayCalc.js
@@ -12,9 +12,50 @@ function MainPropagationDelayCalc() {
       </Typography>
       <hr />
       <br />
-      {/* Write your code here */}
-
-      {/* End your code here */}
+      <TextField
+        label="Distance (meters)"
+        variant="outlined"
+        value={distance}
+        onChange={handleDistanceChange}
+      />
+      <br />
+      <br />
+      <TextField
+        label="Speed of Propagation (m/s)"
+        variant="outlined"
+        value={speed}
+        onChange={handleSpeedChange}
+      />
+      <br />
+      <br />
+      <TextField
+        label="Propagation Delay (seconds)"
+        variant="outlined"
+        value={delay}
+        onChange={handleDelayChange}
+      />
+      <br />
+      <br />
+      <Button variant="contained" color="primary" onClick={calculateDelay}>
+        Calculate
+      </Button>
+      <br />
+      <br />
+      <Typography variant="h6" sx={{ textAlign: "center" }}>
+        {error ? (
+          <Typography color="error" sx={{ textAlign: "center" }}>
+            {error}
+          </Typography>
+        ) : (
+          `${resultType}: ${
+            resultType === "Propagation Delay (seconds)"
+              ? delay
+              : resultType === "Distance (meters)"
+              ? distance
+              : speed
+          }`
+        )}
+      </Typography>
     </Container>
   );
 }
