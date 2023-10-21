389b1817bc8bd5eb13ab253db7b40acbd69f0434
munfaridzahera <munfaridzahera@gmail.com>
2023-10-21 18:55:32 +0530

    added documentation of Quadratic equation intersaction, polynomial equation root finder and set operation

---
+++ calculators/src/mathematical/polyEqRootFinder/InfoPolyEqRootFinder.js
@@ -0,0 +1,74 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  // width: 500,
+  bgcolor: 'background.paper',
+  border: '1px solid #000',
+  boxShadow: 24,
+  p: 3,
+};
+
+{/* For reference look into the following file - calc_for_everything\calculators\src\mathematical\simple_interest\InfoSI.js */}
+// Change the name of the function as the file name
+export default function InfoPolyEqRootFinder() {
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <span style={{cursor: 'pointer'}}>
+      <InfoOutlinedIcon color='primary' onClick={handleOpen}/>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        {/* For reference look into the following file - calc_for_everything\calculators\src\mathematical\simple_interest\InfoSI.js */}
+        <Box sx={style} sm={1}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            {/* Calculator Heading */}
+            Cube Root And Square Root Calculator
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            {/* Definition */}
+            A polynomial equation root finder is a computational tool or algorithm used to determine the values of the variable that make the polynomial equation equal to zero, known as its roots or solutions.
+            <br/>
+            {/* Replace the below calculator name with yuor calculator name */}
+            The formula for calculating Roots of a polynomial equation is:
+            ax 
+n
+ +bx 
+(n−1)
+ +cx 
+(n−2)
+ +…+k=0
+            <br/>
+            <code style={{display: 'flex', justifyContent: 'center'}}>
+              <h3>
+                {/* Add calc formula */}
+                
+              </h3>
+            </code>
+            <code style={{display: 'flex', justifyContent: 'center'}}>
+              <h5>
+                {/* Define meaning that each variable convey */}
+                The coefficients "a," "b," and "c" are constants that determine the specific characteristics of the polynomial equation.
+                x is the variable, and you would solve for the values of "x" that make the equation true.   
+              </h5>
+            </code>
+          </Typography>
+        </Box>
+      </Modal>
+    </span>
+  );
+}
