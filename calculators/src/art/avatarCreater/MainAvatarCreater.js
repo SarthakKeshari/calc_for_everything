92a15c433fadbc1f553fd2733db587fe2dac0be6
Upadhyay-Harshit <upadhyayharshit@icloud.com>
2023-10-21 20:10:44 +0530

    Added MainAvatarCreater

--- calculators/src/art/avatarCreater/MainAvatarCreater.js
+++ calculators/src/art/avatarCreater/MainAvatarCreater.js
@@ -1,19 +1,47 @@
-import React from 'react';
-import { Container, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import Avatar from '@mui/material/Avatar';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
 
-function MainAvatarCreater(){
-    return(
-        <Container maxWidth="lg" sx={{ bgcolor: '#eeeeee', minHeight: '90vh', paddingY:"10" }}>
-            <Typography pt={1} variant='h5' sx = {{textAlign: "center"}}>Avatar Creater</Typography>
-            <hr/>
-            <br/>
-            {/* Write your code here */}
+function AvatarCreator() {
+    const [initials, setInitials] = useState('');
+    const [avatarSrc, setAvatarSrc] = useState('');
 
+    const handleInitialsChange = (e) => {
+        setInitials(e.target.value);
+    };
 
+    const createAvatar = () => {
+        // You can customize the avatar color, background, and size as per your requirements
+        const avatar = (
+            <Avatar style={{ backgroundColor: '#3f51b5', width: 100, height: 100 }}>
+                {initials}
+            </Avatar>
+        );
+        setAvatarSrc(avatar);
+    };
 
-            {/* End your code here */}
-        </Container>
-    )
+    return (
+        <div>
+            <Grid container spacing={2}>
+                <Grid item>
+                    <TextField
+                        label="Enter Initials"
+                        variant="outlined"
+                        value={initials}
+                        onChange={handleInitialsChange}
+                    />
+                </Grid>
+                <Grid item>
+                    <Button variant="contained" color="primary" onClick={createAvatar}>
+                        Create Avatar
+                    </Button>
+                </Grid>
+            </Grid>
+            <div style={{ marginTop: '20px' }}>{avatarSrc}</div>
+        </div>
+    );
 }
 
-export default MainAvatarCreater;
+export default AvatarCreator;
