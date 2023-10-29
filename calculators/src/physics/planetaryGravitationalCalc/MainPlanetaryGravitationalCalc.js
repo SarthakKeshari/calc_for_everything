33706e2a01b1bd59e4b7e23805b68d5175548d88
wuyu <viafuture@outlook.com>
2023-10-29 21:34:14 +0800

    Planet Gravity Calculator

--- calculators/src/physics/planetaryGravitationalCalc/MainPlanetaryGravitationalCalc.js
+++ calculators/src/physics/planetaryGravitationalCalc/MainPlanetaryGravitationalCalc.js
@@ -68,23 +68,3 @@ function PlanetGravityCalculator() {
 export default PlanetGravityCalculator;
 
 
-// import React from 'react';
-// import { Container, Typography } from '@mui/material';
-
-// function MainPlanetaryGravitationalCalc(){
-//     return(
-//         <Container maxWidth="lg" sx={{ bgcolor: '#eeeeee', minHeight: '90vh', paddingY:"10" }}>
-//             <Typography pt={1} variant='h5' sx = {{textAlign: "center"}}>Planetary Gravitational Calculator</Typography>
-//             <hr/>
-//             <br/>
-//             {/* Write your code here */}
-
-
-
-//             {/* End your code here */}
-//         </Container>
-//     )
-// }
-
-// export default MainPlanetaryGravitationalCalc;
-
