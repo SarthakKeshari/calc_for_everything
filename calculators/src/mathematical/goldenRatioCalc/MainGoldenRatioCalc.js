71854045def57bfe2b3cef41bbfeb2b48f74beeb
Xie Junting <1520229462@qq.com>
2023-10-30 17:55:57 +0800

    ss

--- calculators/src/mathematical/goldenRatioCalc/MainGoldenRatioCalc.js
+++ calculators/src/mathematical/goldenRatioCalc/MainGoldenRatioCalc.js
@@ -8,7 +8,25 @@ function MainGoldenRatioCalc(){
             <hr/>
             <br/>
             {/* Write your code here */}
+            const [a, setA] = useState('');
+            const [b, setB] = useState('');
+            const [result, setResult] = useState('');
 
+            const calculateGoldenRatio = () => {
+                if (a && b) {
+                    const aValue = parseFloat(a);
+            const bValue = parseFloat(b);
+
+            if (bValue !== 0) {
+                        const ratio = (aValue + bValue) / aValue;
+            setResult(`The golden ratio is ${ratio.toFixed(2)}`);
+                    } else {
+                        setResult("Error: B value cannot be zero.");
+                    }
+                } else {
+                    setResult("Error: Please enter values for A and B.");
+                }
+            };
 
 
             {/* End your code here */}
