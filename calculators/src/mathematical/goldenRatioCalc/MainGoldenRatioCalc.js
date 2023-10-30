53ac4df1810343c9415a0293468ada67f996a920
Xie Junting <1520229462@qq.com>
2023-10-30 20:22:05 +0800

    2

--- calculators/src/mathematical/goldenRatioCalc/MainGoldenRatioCalc.js
+++ calculators/src/mathematical/goldenRatioCalc/MainGoldenRatioCalc.js
@@ -1,34 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Typography } from '@mui/material';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
-function MainGoldenRatioCalc(){
+function MainGoldenRatioCalc() {
+
+    const [a, setA] = useState('');
+    const [b, setB] = useState('');
+    const [ratioType, setRatioType] = useState('');
+    const [missingRatio, setMissingRatio] = useState('');
+
+    const calculateGoldenRatio = (a, b) => {
+        if (a === 0 || b === 0) {
+            return null;
+        }
+        const ratio = (a + b) / a;
+        return ratio;
+    };
+
+    const calculateMissingRatio = () => {
+        const parsedA = parseFloat(a);
+        const parsedB = parseFloat(b);
+
+        if (isNaN(parsedA) || isNaN(parsedB)) {
+            setMissingRatio('Invalid input');
+            return;
+        }
+
+        if (ratioType === 'AB') {
+            const missingRatio = parsedB * (1 + calculateGoldenRatio(parsedA, parsedB));
+            setMissingRatio(missingRatio.toFixed(2));
+           
+        } else if (ratioType === 'BA') {
+            const missingRatio = parsedA * calculateGoldenRatio(parsedA, parsedB) - parsedB;
+            setMissingRatio(missingRatio.toFixed(2));
+        } else {
+            setMissingRatio('Invalid ratio type');
+        }
+    };
+
     return(
         <Container maxWidth="lg" sx={{ bgcolor: '#eeeeee', minHeight: '90vh', paddingY:"10" }}>
             <Typography pt={1} variant='h5' sx = {{textAlign: "center"}}>Golden Ratio Calculator</Typography>
             <hr/>
             <br/>
             {/* Write your code here */}
-            const [a, setA] = useState('');
-            const [b, setB] = useState('');
-            const [result, setResult] = useState('');
-
-            const calculateGoldenRatio = () => {
-                if (a && b) {
-                    const aValue = parseFloat(a);
-            const bValue = parseFloat(b);
-
-            if (bValue !== 0) {
-                        const ratio = (aValue + bValue) / aValue;
-            setResult(`The golden ratio is ${ratio.toFixed(2)}`);
-                    } else {
-                        setResult("Error: B value cannot be zero.");
-                    }
-                } else {
-                    setResult("Error: Please enter values for A and B.");
-                }
-            };
-
-
+            
+            
+            <TextField
+                label="Value of A"
+                variant="outlined"
+                value={a}
+                onChange={(e) => setA(e.target.value)}
+                fullWidth
+                margin="normal"
+            />
+            <TextField
+                label="Value of B"
+                variant="outlined"
+                value={b}
+                onChange={(e) => setB(e.target.value)}
+                fullWidth
+                margin="normal"
+            />
+            <TextField
+                label="Ratio Type (AB or BA)"
+                variant="outlined"
+                value={ratioType}
+                onChange={(e) => setRatioType(e.target.value)}
+                fullWidth
+                margin="normal"
+            />
+            <Button variant="contained" onClick={calculateMissingRatio}>
+                Calculate Missing Golden Ratio
+            </Button>
+            {missingRatio && (
+                <Typography variant="h6" sx={{ marginTop: '2rem', textAlign: 'center' }}>
+                    Missing Golden Ratio: {missingRatio}
+                </Typography>
+            )}
             {/* End your code here */}
         </Container>
     )
