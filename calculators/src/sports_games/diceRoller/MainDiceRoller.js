c719d5b419464ad3620dbb102564d728032a9ed5
Upadhyay-Harshit <upadhyayharshit@icloud.com>
2023-10-31 23:48:53 +0530

    Added dice roller

--- calculators/src/sports_games/diceRoller/MainDiceRoller.js
+++ calculators/src/sports_games/diceRoller/MainDiceRoller.js
@@ -1,19 +1,66 @@
-import React from 'react';
-import { Container, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Container, Button, Grid, Typography } from '@mui/material';
 
-function MainDiceRoller(){
-    return(
-        <Container maxWidth="lg" sx={{ bgcolor: '#eeeeee', minHeight: '90vh', paddingY:"10" }}>
-            <Typography pt={1} variant='h5' sx = {{textAlign: "center"}}>Dice Roller</Typography>
-            <hr/>
-            <br/>
-            {/* Write your code here */}
+function MainDiceRoller() {
+    const [results, setResults] = useState([]);
 
+    const rollDice = (numDice) => {
+        const newResults = [];
+        for (let i = 0; i < numDice; i++) {
+            const result = Math.floor(Math.random() * 6) + 1;
+            
+            newResults.push(result);
+        }
+        setResults(newResults);
+    };
 
-
-            {/* End your code here */}
+    return (
+        <Container>
+            <h1>Dice Roller</h1>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <Button variant="contained" color="primary" onClick={() => rollDice(1)}>
+                        Roll 1 Die
+                    </Button>
+                </Grid>
+                <Grid item xs={12}>
+                    <Button variant="contained" color="primary" onClick={() => rollDice(2)}>
+                        Roll 2 Dice
+                    </Button>
+                </Grid>
+                <Grid item xs={12}>
+                    <Button variant="contained" color="primary" onClick={() => rollDice(3)}>
+                        Roll 3 Dice
+                    </Button>
+                </Grid>
+                <Grid item xs={12}>
+                    <Button variant="contained" color="primary" onClick={() => rollDice(4)}>
+                        Roll 4 Die
+                    </Button>
+                </Grid>
+                <Grid item xs={12}>
+                    <Button variant="contained" color="primary" onClick={() => rollDice(5)}>
+                        Roll 5 Die
+                    </Button>
+                </Grid>
+                <Grid item xs={12}>
+                    <Button variant="contained" color="primary" onClick={() => rollDice(6)}>
+                        Roll 6 Die
+                    </Button>
+                </Grid>
+            </Grid>
+            <Typography variant="h6" gutterBottom>
+                Results:
+            </Typography>
+            <div>
+                {results.map((result, index) => (
+                    <Typography key={index} variant="subtitle1">
+                        Die {index + 1}: {result}
+                    </Typography>
+                ))}
+            </div>
         </Container>
-    )
+    );
 }
 
 export default MainDiceRoller;
