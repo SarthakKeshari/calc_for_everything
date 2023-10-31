361ecae569cebc9d1c5de3e592f06653aad94806
ShivAnand Y <shivay4318@gmail.com>
2023-10-31 23:15:22 +0530

    feat: implement external angle of each side #608

--- calculators/src/mathematical/externalAnglesOfAPolygonCalc/MainExternalAnglesOfAPolygonCalc.js
+++ calculators/src/mathematical/externalAnglesOfAPolygonCalc/MainExternalAnglesOfAPolygonCalc.js
@@ -1,18 +1,43 @@
-import React from 'react';
-import { Container, Typography } from '@mui/material';
+import React, {useState} from 'react';
+import { Container, Typography,TextField,Button,Box } from '@mui/material';
 
 function MainExternalAnglesOfAPolygonCalc(){
-    return(
-        <Container maxWidth="lg" sx={{ bgcolor: '#eeeeee', minHeight: '90vh', paddingY:"10" }}>
-            <Typography pt={1} variant='h5' sx = {{textAlign: "center"}}>External Angles Of a Polygon Calculator</Typography>
-            <hr/>
-            <br/>
-            {/* Write your code here */}
+    const [numberOfSides, setNumberOfSides] = useState('');
+    const [angleOfEachExternalSide, setExternalAngle] = useState(null);
+  
+    const handleCalculate = () => {
 
-
-
-            {/* End your code here */}
-        </Container>
+        const externalAngle = (360/numberOfSides);
+        setExternalAngle(externalAngle.toFixed(2));     
+    };
+  
+    return (
+      <Container maxWidth="lg" sx={{ bgcolor: '#eeeeee', minHeight: '90vh', paddingY: '10' }}>
+        <Typography pt={1} variant="h5" sx={{ textAlign: 'center' }}>
+          External Angles of a Polygon Calculator
+        </Typography>
+        <hr />
+        <br />
+  
+        <TextField
+          label="Number of Sides"
+          fullWidth
+          value={numberOfSides}
+          onChange={(e) => setNumberOfSides(e.target.value)}
+          type="number"
+          inputProps={{ step: 'any' }}
+        />
+  
+        <Button variant="contained" color="primary" onClick={handleCalculate}>
+          Calculate
+        </Button>
+  
+        {angleOfEachExternalSide!== null && (
+          <Box mt={2}>
+            <Typography>Measure of Each Exterior Angle: {angleOfEachExternalSide}°</Typography>
+          </Box>
+        )}
+      </Container>
     )
 }
 
