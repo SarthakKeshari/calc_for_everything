56065a5667d9f394e40038e5611d1e30c61a98e0
ShivAnand Y <shivay4318@gmail.com>
2023-10-31 22:08:20 +0530

    feat: implement the Co-interior Pairs calculator #609

--- calculators/src/mathematical/coInteriorPairs/MainCoInteriorPairs.js
+++ calculators/src/mathematical/coInteriorPairs/MainCoInteriorPairs.js
@@ -1,19 +1,44 @@
-import React from 'react';
-import { Container, Typography } from '@mui/material';
+import React, { useState } from "react";
+import { Container, Typography, TextField, Button, Box } from "@mui/material";
 
 function MainCoInteriorPairs(){
-    return(
-        <Container maxWidth="lg" sx={{ bgcolor: '#eeeeee', minHeight: '90vh', paddingY:"10" }}>
-            <Typography pt={1} variant='h5' sx = {{textAlign: "center"}}>Co-Interior Pairs Calculator</Typography>
-            <hr/>
-            <br/>
-            {/* Write your code here */}
+  const [angle, setAngle] = useState("");
+  const [result, setResult] = useState(null);
 
+  const handleCalculate = () => {
+    // Assuming the sum of co-interior angles is always 180 degrees
+    const otherAngle = 180 - parseFloat(angle);
+    setResult(otherAngle.toFixed(2)); 
+  };
+  return(
+    <Container maxWidth="lg" sx={{ bgcolor: "#eeeeee", minHeight: "90vh", paddingY: "10" }}>
+      <Typography pt={1} variant="h5" sx={{ textAlign: "center" }}> Co-Interior Pairs Calculator </Typography>
+      <hr />
+      <br />
+      {/* Write your code here */}
 
+      <TextField
+        label="Enter first Angle"
+        fullWidth
+        value={angle}
+        onChange={(e) => setAngle(e.target.value)}
+        type="number"
+        inputProps={{ step: "any" }}
+      />
 
-            {/* End your code here */}
-        </Container>
-    )
+      <Button variant="contained" color="primary" onClick={handleCalculate}>
+        Calculate
+      </Button>
+
+      {result !== null && (
+        <Box mt={2}>
+          <Typography>Second Angle: {result}°</Typography>
+        </Box>
+      )}
+
+      {/* End your code here */}
+    </Container>
+  );
 }
 
 export default MainCoInteriorPairs;
