58c2fd0c16d8ae0cd03bf15257397b128fd358f0
Shantanu-Tiwari <shantanutiwari240704@gmail.com>
2023-10-31 23:59:33 +0530

    roofing

--- calculators/src/finance/roofingCalc/MainRoofingCalc.js
+++ calculators/src/finance/roofingCalc/MainRoofingCalc.js
@@ -1,19 +1,72 @@
-import React from 'react';
-import { Container, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Container, Typography, TextField, Button } from '@mui/material';
 
-function MainRoofingCalc(){
-    return(
-        <Container maxWidth="lg" sx={{ bgcolor: '#eeeeee', minHeight: '90vh', paddingY:"10" }}>
-            <Typography pt={1} variant='h5' sx = {{textAlign: "center"}}>Roofing Calculator</Typography>
+function MainRoofingCalc() {
+    // Input fields
+    const [Housebaselength, setHousebaselength] = useState(0);
+    const [Housebasewidth, setHousebasewidth] = useState(0);
+    const [Roofpitch, setRoofpitch] = useState(0);
+    const [Eaves, setEaves] = useState(0);
+    const [Price, setPrice] = useState(1);
+
+    // Output fields
+    const [RoofingArea, setRoofingArea] = useState(0);
+
+    // Calculation
+    const calcRoofingArea = () => {
+        const roofpitchInches = Roofpitch / 12;
+        const roofpitchSquared = Math.pow(roofpitchInches, 2);
+        const adjustedRoofpitch = Math.sqrt(roofpitchSquared + 1);
+
+        // Calculating area
+        const Housebasearea = Housebasewidth * (Housebaselength + Eaves);
+        const roofingArea = Housebasearea * adjustedRoofpitch;
+        setRoofingArea(roofingArea);
+    }
+
+    return (
+        <Container>
+            <Typography variant="h5">Roofing Calculator</Typography>
+
+            <TextField
+                label="House Base Length"
+                type="number"
+                value={Housebaselength}
+                onChange={(e) => setHousebaselength(parseFloat(e.target.value))}
+            />
             <hr/>
             <br/>
-            {/* Write your code here */}
+            <TextField
+                label="House Base Width"
+                type="number"
+                value={Housebasewidth}
+                onChange={(e) => setHousebasewidth(parseFloat(e.target.value))}
+            />
+            <hr/>
+            <br/>
+            <TextField
+                label="Roof Pitch (inches per foot)"
+                type="number"
+                value={Roofpitch}
+                onChange={(e) => setRoofpitch(parseFloat(e.target.value))}
+            />
+            <hr/>
+            <br/>
+            <TextField
+                label="Eaves"
+                type="number"
+                value={Eaves}
+                onChange={(e) => setEaves(parseFloat(e.target.value))}
+            />
+            <hr/>
+            <br/>
+            <Button variant="contained" color="primary" onClick={calcRoofingArea}>
+                Calculate
+            </Button>
 
-
-
-            {/* End your code here */}
+            <Typography variant="h6">Roofing Area: {RoofingArea} square feet</Typography>
         </Container>
-    )
+    );
 }
 
 export default MainRoofingCalc;
