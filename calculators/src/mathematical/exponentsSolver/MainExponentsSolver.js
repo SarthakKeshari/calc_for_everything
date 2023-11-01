e9a30c67da57074c479d563ce7d0a843763c0cd9
wuhue <1778879791@qq.com>
2023-11-01 10:15:31 +0800

    Update MainExponentsSolver.js
    I have fixed some vulnerabilities.

--- calculators/src/mathematical/exponentsSolver/MainExponentsSolver.js
+++ calculators/src/mathematical/exponentsSolver/MainExponentsSolver.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { TextField, Button, Box } from "@mui/material";
+import { TextField, Button, Box, Typography } from "@mui/material";
 
 function MainExponentsSolver() {
   const [input, setInput] = useState("");
