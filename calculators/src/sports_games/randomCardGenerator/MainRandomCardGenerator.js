6fb3fdff36e44ef0984c120d072b0e5fab1d084c
Shivam Patel <87613778+ShivamPatel-24@users.noreply.github.com>
2023-11-23 16:51:04 -0500

    add comments

--- calculators/src/sports_games/randomCardGenerator/MainRandomCardGenerator.js
+++ calculators/src/sports_games/randomCardGenerator/MainRandomCardGenerator.js
@@ -2,9 +2,11 @@ import React, { useState } from 'react';
 import { Container, Typography } from '@mui/material';
 
 const RandomCardGenerator = () => {
+  // Arrays to represent suits and ranks of playing cards
   const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
   const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 
+  // Function to get a random playing card
   const getRandomCard = () => {
     const randomSuit = suits[Math.floor(Math.random() * suits.length)];
     const randomRank = ranks[Math.floor(Math.random() * ranks.length)];
@@ -12,19 +14,26 @@ const RandomCardGenerator = () => {
     return `${randomRank} of ${randomSuit}`;
   };
 
+  // State to store the generated card
   const [generatedCard, setGeneratedCard] = useState('');
 
+  // Function to generate a random card and update the state
   const generateRandomCard = () => {
     const card = getRandomCard();
     setGeneratedCard(card);
   };
 
   return (
-    <Container maxWidth="lg" sx={{ bgcolor: '#eeeeee', minHeight: '90vh', paddingY:"10" }}>
-        <Typography pt={1} variant='h5' sx = {{textAlign: "center"}}>Random Card Generator</Typography>
-        <button onClick={generateRandomCard}>Generate Card</button>
-        {generatedCard && <p>Generated Card: {generatedCard}</p>}
-      </Container>
+    <Container maxWidth="lg" sx={{ bgcolor: '#eeeeee', minHeight: '90vh', paddingY: "10" }}>
+      {/* Heading for the card generator */}
+      <Typography pt={1} variant='h5' sx={{ textAlign: "center" }}>Random Card Generator</Typography>
+      
+      {/* Button to generate a random card */}
+      <button onClick={generateRandomCard}>Generate Card</button>
+      
+      {/* Display the generated card if available */}
+      {generatedCard && <p>Generated Card: {generatedCard}</p>}
+    </Container>
   );
 };
 
