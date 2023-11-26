c9c74e067d885cc47ef62965f2d3689fc557f710
Shivam Patel <87613778+ShivamPatel-24@users.noreply.github.com>
2023-11-25 20:12:59 -0500

    fix a ui issue and add comments

--- calculators/src/computer_science/binaryTreeVisualizer/MainBinaryTreeVisualizer.js
+++ calculators/src/computer_science/binaryTreeVisualizer/MainBinaryTreeVisualizer.js
@@ -5,22 +5,34 @@ import AVLTree from 'avl';
 
 function MainBinaryTreeVisualizer() {
     const [inputValue, setInputValue] = useState('');
-    const [avlTree, setAvlTree] = useState(new AVLTree());
+    
+    // Initialize the AVL tree with the value 10
+    const initialAvlTree = new AVLTree();
+    initialAvlTree.insert(10);
+    
+    const [avlTree, setAvlTree] = useState(initialAvlTree);
     const [binarySearchTree, setBinarySearchTree] = useState(null);
 
+    useEffect(() => {
+        updateBinarySearchTree();
+    }, []);
+
+    // Handles changes in the input field
     const handleInputChange = (e) => {
         setInputValue(e.target.value);
     };
 
+    // Handles the addition of a new node
     const handleAddNode = () => {
         const value = parseInt(inputValue, 10);
         if (!isNaN(value)) {
-            avlTree.insert(value);
-            setInputValue('');
-            updateBinarySearchTree();
+            avlTree.insert(value); // Insert into AVL tree for balancing
+            setInputValue(''); // Reset input value
+            updateBinarySearchTree(); // Update the visualized binary search tree
         }
     };
 
+    // Recursively builds a balanced binary search tree from a sorted list
     const buildBalancedBST = (sortedList) => {
         if (sortedList.length === 0) {
             return null;
@@ -28,21 +40,23 @@ function MainBinaryTreeVisualizer() {
 
         const medianIndex = Math.floor(sortedList.length / 2);
         const node = new BinarySearchTreeNode(sortedList[medianIndex]);
-
+        // Build left and right subtrees recursively
         node.left = buildBalancedBST(sortedList.slice(0, medianIndex));
         node.right = buildBalancedBST(sortedList.slice(medianIndex + 1));
 
         return node;
     };
 
+    // Updates the binary search tree used for visualization
     const updateBinarySearchTree = () => {
         const sortedList = [];
-        avlTree.forEach((node) => sortedList.push(node.key));
+        avlTree.forEach((node) => sortedList.push(node.key)); // Get sorted list from AVL tree
 
-        const newTree = buildBalancedBST(sortedList);
-        setBinarySearchTree(newTree);
+        const newTree = buildBalancedBST(sortedList); // Build a balanced BST
+        setBinarySearchTree(newTree); // Update the state
     };
 
+    // Redraw the binary search tree whenever it changes
     useEffect(() => {
         if (binarySearchTree) {
             drawBinaryTree(binarySearchTree, document.querySelector('#binaryTreeCanvas'), {
@@ -51,6 +65,7 @@ function MainBinaryTreeVisualizer() {
         }
     }, [binarySearchTree]);
 
+    // Render the component
     return (
         <Container maxWidth="lg" sx={{ bgcolor: '#eeeeee', minHeight: '90vh', paddingY: "10" }}>
             <Typography pt={1} variant='h5' sx={{ textAlign: "center" }}>Binary Tree Visualizer</Typography>
